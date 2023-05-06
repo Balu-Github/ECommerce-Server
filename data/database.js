3c5c2f89faf6c5bdd90bca298ce0983d2d96d5ce
Balakrishna Barre <balakrishna.b3@outlook.com>
2023-05-06 14:35:01 +0530

    db name changed

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "UdemyCourse",
+      dbName: "ECommerceApp",
     });
 
     console.log(`Server connected to database ${connection.host}`);
